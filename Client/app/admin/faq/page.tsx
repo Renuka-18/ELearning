add4f360da75c6713365369281f91bbc8420aedb
G Renuka Devi <87711293+Renuka-18@users.noreply.github.com>
2024-06-07 20:00:21 +0530

    Add files via upload

---
+++ Client/app/admin/faq/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import AdminSidebar from "@/app/components/admin/AdminSidebar";
+import DashboardHero from "@/app/components/admin/DashboardHero";
+import AdminProtected from "@/app/hooks/adminProtected";
+import Heading from "@/app/utils/Headings";
+import React from "react";
+import EditFaq from "@/app/components/admin/customization/Editfaq";
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>
+        <AdminProtected>
+        <Heading
+        title="ELearning-admin"
+        description="Online platform to get help from teachers"
+        keywords="program mern rjs"
+        />
+        <div className="flex h-screen">
+            <div className="1500px:w-[16%] w-1/5">
+                <AdminSidebar/>
+            </div>
+            <div className="w-[85%]">
+                <DashboardHero/>
+                <EditFaq/>  
+            </div>
+        </div>
+        </AdminProtected>
+    </div>
+  );
+};
+
+export default page;
