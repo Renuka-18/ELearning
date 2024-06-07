8492cb44bab11da007deb9b6af0c89ec9380cbd4
G Renuka Devi <87711293+Renuka-18@users.noreply.github.com>
2024-06-07 20:29:10 +0530

    Add files via upload

---
+++ Server/services/order.service.ts
@@ -0,0 +1,26 @@
+import { NextFunction , Response} from "express";
+import { catchAsyncError } from "../middleware/catchAsyncError";
+import OrderModel from "../models/order.model";
+import CourseModel from "../models/course.models";
+
+
+export const newOrder = catchAsyncError(async(data:any, next:NextFunction, res:Response)=>{
+    const order = await OrderModel.create(data);
+    
+    res.status(201).json({
+        success:true,
+        order,
+      })
+   
+})
+
+export const getAllOrdersService = async(res:Response) => {
+  const orders = await OrderModel.find().sort({
+      createdAt:-1
+  });
+  res.status(201).json({
+      success:true,
+      orders
+  });
+};
+
