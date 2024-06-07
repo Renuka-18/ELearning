5da1ca9301dd19e87d2dafff42f522d21f3962cb
G Renuka Devi <87711293+Renuka-18@users.noreply.github.com>
2024-06-07 19:37:19 +0530

    Add files via upload

---
+++ Client/course-access/page.tsx
@@ -0,0 +1,45 @@
+"use client"
+import Loader from '@/app/components/Loader/Loader';
+import CourseContent from '@/app/components/course/CourseContent';
+
+import { useLoadUserQuery } from '@/redux/features/api/apiSlice';
+import { redirect } from 'next/navigation';
+import React, { useEffect } from 'react'
+
+type Props = {
+    params:any;
+}
+
+const page = ({params}: Props) => {
+    const id = params.id;
+
+    const {isLoading,error,data} = useLoadUserQuery(undefined,{});
+    useEffect(()=>{
+        if(data){
+            const isPurchased = data.user.courses.find((item:any) => item._id === id);
+            if(!isPurchased){
+                redirect("/")
+            }
+            if(error){
+                redirect("/");
+            }
+        }
+
+    },[data,error]);
+
+  return (
+    <>
+    {
+        isLoading ? (
+            <Loader/>
+        ) : (
+            <div >
+                < CourseContent id={id} />
+            </div>
+        )
+    }
+    </>
+  )
+}
+
+export default page
