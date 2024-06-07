a3fa40a57548a7cf7470728817091845b40c953b
G Renuka Devi <87711293+Renuka-18@users.noreply.github.com>
2024-06-07 20:09:31 +0530

    Add files via upload

---
+++ Client/app/hooks/adminProtected.tsx
@@ -0,0 +1,21 @@
+"use client";
+import {redirect} from "next/navigation";
+import React from "react";
+import { useSelector } from "react-redux";
+import UserAuth from "./userAuth";
+
+
+interface ProtectedProps{
+    children: React.ReactNode
+}
+
+export default function AdminProtected({ children } :ProtectedProps){
+    const {user} = useSelector((state:any) => state.auth);
+    if(user){
+    const isAdmin = user?.role === "admin";
+    return isAdmin ? children:redirect("/");
+    }
+    
+}
+
+
---
+++ Client/app/hooks/userAuth.tsx
@@ -0,0 +1,13 @@
+import { truncate } from "fs";
+import { useSelector } from "react-redux";
+
+export default function UserAuth() {
+    const {user} = useSelector((state:any) => state.auth);
+
+        if(user){
+            return true;
+        }
+        else{
+            return false;
+        }
+}
